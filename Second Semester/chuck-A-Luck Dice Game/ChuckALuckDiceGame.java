17412be197c4a05271f49914343c771bb9867e44
Ahmed Mahdi <72886944+ahmed-mahdi18@users.noreply.github.com>
2021-03-02 17:56:22 +0000

    Update ChuckALuckDiceGame.java

--- Second Semester/chuck-A-Luck Dice Game/ChuckALuckDiceGame.java
+++ Second Semester/chuck-A-Luck Dice Game/ChuckALuckDiceGame.java
@@ -1,36 +1,3 @@
-/* SELF ASSESSMENT
-
-1. ResolveBet
-
-I have correctly defined ResolveBet which takes the bet type (String) and the Wallet object, and a void return type [Mark out of 7: 7].
-Comment: resolvebet takes in String betType and the wallet object and it is a void
-My program presents the amount of cash in the wallet and asks the user how much he/she would like to bet [Mark out of 8:8 ].
-Comment: yes it does
-My program ensures the bet amount is not greater than the cash in the wallet [Mark out of 5: 5].
-Comment: yes it does by using the get method in Wallet.java
-My program creates three Dice objects, rolls them and creates a total variable with a summation of the roll values returned [Mark out of 15: 15]..
-Comment: yes my program does that
-My program determines the winnings by comparing the bet type with the total and comparing the bet type with the dice faces for the triple bet [Mark out of 20: 20 ].
-Comment: yes my program compares them
-My program outputs the results (win or loss) and adds the winnings to the wallet if user wins or removes the bet amount from the wallet if the user loses [Mark out of 10:10 ].
-Comment: yes it does
-
-2. Main
-
-I ask the user for the amount of cash he/she has, create a Wallet object and put this cash into it [Mark out of 15: 15 ]
-Comment: yes i create a wallet object and use the put method in Wallet.java
-My program loops continuously until the user either enters quit or the cash in the wallet is 0 [Mark out of 5:5 ]
-Comment: yes it does
-I ask the user to enter any of the four bet types or quit [Mark out of 5: 5].
-Comment: yes,my program asks for the user input
-My program calls resolveBet for each bet type entered [Mark out of 5:5 ].
-Comment: yes it does
-At the end of the game my program presents a summary message regarding winnings and losses [Mark out of 5: 5 ]
-Comment: yes my program prints out a summary
-
- Total Mark out of 100 (Add all the previous marks): 100
-*/
-
 
 import java.util.Scanner;
 
